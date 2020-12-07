f39dacf03af1e57142193f0c014165b0db2f5de2
JSegoviaa <acosta.segoviaa@hotmail.com>
2020-12-07 11:32:22 -0500

    se desinstaló jquery y popperjs

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,76 +1,51 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 import logo from '../../assets/logo.jpg';
 
-const Navbar = () => {
+const NavBar = () => {
   return (
-    <nav
-      id="menu"
-      className="container navbar navbar-expand-lg font-weight-bold"
-    >
+    <Navbar className="container" expand="lg">
       <Fade left>
-        <NavLink to="./" className="navbar-brand">
-          <img
-            src={logo}
-            width="130"
-            height="auto"
-            className="d-inline-block align-top"
-            alt="Jabonarte"
-          />
+        <NavLink to="./">
+          <Navbar.Brand>
+            <img
+              src={logo}
+              width="130"
+              height="auto"
+              className="d-inline-block align-top"
+              alt="Jabonarte"
+            />
+          </Navbar.Brand>
         </NavLink>
       </Fade>
-      <button
-        className="navbar-toggler navbar-light"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div
-        className="collapse navbar-collapse justify-content-end"
-        id="navbarNav"
-      >
-        <ul className="navbar-nav font-body">
-          <Fade right>
-            <li className="nav-item nav-border-l">
-              <NavLink to="./productos" className="nav-link nav-link2">
-                Productos
-              </NavLink>
-            </li>
-            <li className="nav-item nav-border-r">
-              <NavLink to="./cursos" className="nav-link nav-link2">
-                Cursos
-              </NavLink>
-            </li>
-            <li className="nav-item nav-border-l nav-border-r">
-              <NavLink to="./sobre-nosotros" className="nav-link nav-link2">
-                ¿Quiénes somos?
-              </NavLink>
-            </li>
-            <li className="nav-item nav-border-r">
-              <NavLink to="./contacto" className="nav-link nav-link2">
-                Contacto
-              </NavLink>
-            </li>
 
-            <li className="nav-dist">
-              <NavLink
-                to="./conviertete-en-distribuidor"
-                className="nav-link btn btn-info"
-              >
-                Conviértete en distribuidor
-              </NavLink>
-            </li>
-          </Fade>
-        </ul>
-      </div>
-    </nav>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Fade right>
+          <NavLink to="./productos" className="nav-link nav-link2">
+            Productos
+          </NavLink>
+          <NavLink to="./cursos" className="nav-link nav-link2">
+            Cursos
+          </NavLink>
+          <NavLink to="./sobre-nosotros" className="nav-link nav-link2">
+            ¿Quiénes somos?
+          </NavLink>
+          <NavLink to="./contacto" className="nav-link nav-link2">
+            Contacto
+          </NavLink>
+          <NavLink
+            to="./conviertete-en-distribuidor"
+            className="nav-link btn btn-info"
+          >
+            Conviértete en distribuidor
+          </NavLink>
+        </Fade>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavBar;
