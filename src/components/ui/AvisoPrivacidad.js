e37941c7d0b2ed71fc7c5f332a8e1083290e67c2
JSegoviaa <acosta.segoviaa@hotmail.com>
2020-12-08 19:09:36 -0500

    se corrigió modal 2.0

--- src/components/ui/AvisoPrivacidad.js
+++ src/components/ui/AvisoPrivacidad.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Container, Modal } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 
 const AvisoPrivacidad = () => {
   const [show, setShow] = useState(false);
@@ -63,55 +63,3 @@ const AvisoPrivacidad = () => {
 };
 
 export default AvisoPrivacidad;
-
-{
-  /* <div>
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-toggle="modal"
-        data-target="#exampleModalLong"
-      >
-        Aviso de privacidad
-      </button>
-
-      <div
-        className="modal fade"
-        id="exampleModalLong"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLongTitle"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLongTitle">
-                Aviso de privacidad
-              </h5>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-primary"
-                data-dismiss="modal"
-              >
-                Cerrar
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> */
-}
